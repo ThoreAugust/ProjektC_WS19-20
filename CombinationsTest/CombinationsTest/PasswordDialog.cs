2c2d03ab71e4b87ec05590383ffcdfae9dcc723c
ThoreAugust <thore.august@haw-hamburg.de>
2019-10-15 16:33:46 +0200

    TA added some functionality to PassHandler WIP

--- CombinationsTest/CombinationsTest/PasswordDialog.cs
+++ CombinationsTest/CombinationsTest/PasswordDialog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,13 @@ namespace CombinationsTest
     public partial class PasswordDialog : Form
     {
         private bool stayOpen;
+        private PasswordHandler pwHandler;
         public PasswordDialog()
         {
+            pwHandler = new PasswordHandler();
             InitializeComponent();
         }
-
+      
         private void okButton_Click(object sender, EventArgs e)
         {
             stayOpen = false;
--- CombinationsTest/CombinationsTest/PasswordHandler.cs
+++ CombinationsTest/CombinationsTest/PasswordHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,44 +11,75 @@ namespace CombinationsTest
     {
         private Dictionary<string, string> passwordHashPair = new Dictionary<string, string>();
 
-        public PasswordHandler(String password)
+        public PasswordHandler()
         {
-            passwordHashPair.Add(password, hashPassword(password));
         }
 
-        public string hashPassword(string password)
+        private void hashPassword(string password)
         {
-            string passwordHash;
-            if (String.IsNullOrEmpty(password))
-                return String.Empty;
-
-            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            string pwHash;
+            if (!String.IsNullOrEmpty(password))
             {
-                byte[] textData = System.Text.Encoding.UTF8.GetBytes(password);
-                byte[] hash = sha.ComputeHash(textData);
-                passwordHash = BitConverter.ToString(hash).Replace("-", String.Empty);
+                using (var sha = new System.Security.Cryptography.SHA256Managed())
+                {
+                    byte[] textData = System.Text.Encoding.UTF8.GetBytes(password);
+                    byte[] hash = sha.ComputeHash(textData);
+                    pwHash = BitConverter.ToString(hash).Replace("-", String.Empty);
+                    passwordHashPair.Add(password, pwHash);
+                }
             }
 
-            //return passwordHash;
-            return "test";
         }
 
-        public void saveHashToLog ()
+        public void savePassToLog ()
         {
             Dictionary<string, string>.ValueCollection values= passwordHashPair.Values;
             foreach (var value in values)
             {
 
             }
+            using (StreamWriter writer = File.CreateText("pwLog.txt"))
+            {
+                writer.WriteLine("[PassHash]");
+                foreach (string value in values)
+                {
+                    writer.WriteLine("cur:"+value);
+                }
+            }
 
         }
-
-        public string readHashFromLog()
+        public void readPassFromLog()
         {
-            string passwordHash = "test";
-
-
-            return passwordHash;
+            if (!File.Exists("pwLog.txt"))
+            {
+                using (StreamWriter writer = File.CreateText("pwLog.txt"))
+                {
+                    writer.WriteLine("[PassHash]");
+                }
+            }
+            else
+            {
+                using (StreamReader reader = File.OpenText("pwLog.txt"))
+                {
+                    string line;
+                    string [] splitLine;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (line != "[PassHash]" && line != "")
+                        {
+                            if (line.Contains("cur:"))
+                            {
+                                splitLine = line.Split(':');
+                                foreach (string item in splitLine)
+                                {
+                                    Console.WriteLine(item);
+                                }
+                                hash = splitLine[1];
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
