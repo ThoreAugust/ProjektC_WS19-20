ca7712fc0240380a1974dd349ba78057333bfbcd
ThoreAugust <thore.august@haw-hamburg.de>
2019-10-15 11:57:10 +0200

    TA Programm and Krategorie class added

---
+++ CombinationsTest/CombinationsTest/Kategorie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsTest
+{
+    class Kategorie
+    {
+        private String name;
+        private double usedTime;
+        private double maxTime;
+        private List<Programm> programs;
+
+        public Kategorie(String n, double ut, double mt, List<Programm> p)
+        {
+            name = n;
+            usedTime = ut;
+            maxTime = mt;
+            programs = p;
+        }
+        override public String ToString()
+        {
+            String p = "";
+            foreach (Programm program in programs)
+                p += program.getPath() + ":";
+            return name + ";" + maxTime + ";" + p;
+        }
+    }
+}
---
+++ CombinationsTest/CombinationsTest/Programm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombinationsTest
+{
+    class Programm
+    {
+        private string name;
+        private string path;
+        private double usedTime;
+        private double maxTime;
+        private string kategorie;
+
+        public Programm(String n, String p, double ut, double mt)
+        {
+            name = n;
+            path = p;
+            usedTime = ut;
+            maxTime = mt;
+        }
+        public void setUsedTime(double newUsedTime)
+        {
+            usedTime = newUsedTime;
+        }
+        public double getUsedTime()
+        {
+            return usedTime;
+        }
+        public string getPath()
+        {
+            return path;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public double getMaxTime()
+        {
+            return maxTime;
+        }
+        public void setMaxTime(double newMaxTime)
+        {
+            maxTime = newMaxTime;
+        }
+        public string getKategorie()
+        {
+            return kategorie;
+        }
+        public void setKategorie(string newKategorie)
+        {
+            kategorie = newKategorie;
+        }
+        public override string ToString()
+        {
+            return name + ";" + path + ";" + usedTime + ";" + maxTime;
+        }
+    }
+}
